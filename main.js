1e63c717d9b8f05183168af0fe4654aa57274406
MedhaManasvini <79033962+MedhaManasvini@users.noreply.github.com>
2022-04-17 11:03:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+    
+}
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+}
+function draw(){
+    
+}
